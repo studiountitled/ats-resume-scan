3f184e0430058ad6d74842e9dfd4c87bc64afcdf
Praise Omogbemeh <onosithena@gmail.com>
2025-02-10 20:03:44 -0800

    add titles to all pages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,22 @@
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="loading-container">
-      <div className="not-found-content">
-        <h2>You look lost. I can change that.</h2>
-        <p>
-          <Link href="/">Go to the homepage</Link>
-        </p>
+    <>
+      <Head>
+        <title>ATS Resume Scanner - Not Found</title>
+      </Head>
+      <div className="loading-container">
+        <div className="not-found-content">
+          <h2>You look lost. I can change that.</h2>
+          <p>
+            <Link href="/">Go to the homepage</Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
